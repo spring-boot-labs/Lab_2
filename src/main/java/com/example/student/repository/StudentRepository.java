f34e1f1ef62d1e0c9d74fe4ab0b7faef4917613d
HoaAng911 <mayanh279@gmail.com>
2026-01-30 22:00:37 +0700

    h

--- src/main/java/com/example/student/repository/StudentRepository.java
+++ src/main/java/com/example/student/repository/StudentRepository.java
@@ -1,9 +1,10 @@
 package com.example.student.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.student.entity.Student;
-
+@Repository
 public interface  StudentRepository 
   extends JpaRepository<Student, Integer>{
 
